d23e06d118c8cbc590e7665306998542ae9ccd05
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-19 14:37:09 -0400

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,59 @@
-console.log('wowow')
-import $ from 'jquery';
+import $ from 'jquery'
+import Backbone from 'backbone'
 
-$.getJSON('/proxy?api=http://www.bbc.co.uk/radio1/playlist.json').then(function(serverRes){
-	console.log(serverRes)
+// import {createRadioTemplate} from './template-radio.js'
+import {createStartingTemplate} from './home-template.js'
+// import {Playlist1Collection, Playlist2Collection} from './_models.js'
+
+
+const AppRouter = Backbone.Router.extend({
+	initialize: function(){
+		console.log('app is routing!')
+		Backbone.history.start()
+	},
+
+  routes: {
+	  'projects' : 'showProjects',
+		'projects/:id' : 'showOneProject',
+		'skills' : 'showSkillsPage',
+		'contact': 'showContactPage',
+		'' : 'showHomePage',
+
+	},
+
+// 	showContactPage: function(){
+// console.log("HELLO Contact PAGE!!!")
+// let infoContainerEl = document.querySelector('.information-container')
+//
+//
+// let playlist2RadioCollection= new Playlist2Collection()
+//
+// playlist2RadioCollection.fetch().then(function(serverRes){
+// 	let secondModelsList= playlist2RadioCollection.models
+// 	let bigHTMLStr= createRadioTemplate(secondModelsList, "Radio2")
+// 	document.querySelector(".information-container").innerHTML= bigHTMLStr
+//
+// })
+//
+// },
+
+
+showHomePage: function(){
+console.log("HELLO HOME PAGE!!!")
+let infoContainerEl = document.querySelector('.information-container')
+
+
+
+// $.getJSON('http://www.bbc.co.uk/radio1/playlist.json').then(function(serverRes){
+// 		let introducingArray = serverRes.playlist.introducing
+		// console.log(introducingArray)
+		infoContainerEl.innerHTML =  createStartingTemplate()
+// })
+},
+
+
+
 })
+
+
+let myPortfolio = new AppRouter()
---
+++ src/scripts/home-template.js
@@ -0,0 +1,16 @@
+
+
+import Backbone from 'backbone';
+
+
+export const createStartingTemplate = function(){
+
+	return `
+	<h1 class= "introducing-title">Hi</h1>
+	<hr/>
+	<div class="jumbotron">
+		<a href="http://pixady.com/image/mk8/"><img src="http://img.pixady.com/2017/03/152601_frontpagefinal_460x307.jpg" alt="" /></a>	
+	</div>
+	`
+
+}
