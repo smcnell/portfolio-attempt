65e3faed700a1bf1b963c969384fb917dc422ee2
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-03-25 17:23:04 -0400

    changes

--- src/scripts/home-template.js
+++ src/scripts/home-template.js
@@ -6,6 +6,7 @@ import Backbone from 'backbone';
 export const createStartingTemplate = function(){
 
 	return `
+
 	<div id="starting" class="starting">
 		<div class="jumbotron birds-jumbo">
 			<img src="./shannonbird2copy.jpg" />
@@ -17,14 +18,28 @@ export const createStartingTemplate = function(){
 	</div>
 	<br/>
 
-	<div id="skills" class="skills">
-		<h1 class= "new-section-skills">Skills</h1>
-		<div class= "skills-container">
-			<div class= "front-end-skills my-skills">
-				<i class="html-icon fa fa-html5" aria-hidden="true"></i>
-				<h1> Front-End Development </h1>
-				<p> I wrote code using HTML5, CSS3 (SCSS), and JavaScript. For recent projects, I enjoy using React, jQuery, and Backbone.</p>
+	<div id="about" class="about-me">
+	<i><i><i><i><i></i></i></i></i></i>
+		<div class= "big-container">
+		<h1> About </h1>
+			<div class= "about">
+				<img src ="./headshotcopy.jpg"/>
+				<p>Hi, I'm Shannon, and I'm a Front-End Engineer.  I am a recent graduate of The Iron Yard of Charleston, where my love for problem-solving and creating took shape in the form of coding for the first time.  I am a former Special Education teacher and will forever be inspired by individuality.</p>
 			</div>
+		</div>
+	</div>
+
+<div id="skills" class="skills">
+		<h1 class= "new-section-skills">Skills</h1>
+
+	<div class= "skills-container">
+
+		<div class= "front-end-skills my-skills">
+			<i class="html-icon fa fa-html5" aria-hidden="true"></i>
+			<h1> Front-End Development </h1>
+			<p> I write code using HTML5, CSS3 (SCSS), and JavaScript. For recent projects, I enjoy using React, jQuery, and Backbone.</p>
+		</div>
+
 
 			<div class= "responsive-skills my-skills">
 			<i class="html-icon fa fa-mobile" aria-hidden="true"></i>
@@ -45,24 +60,14 @@ export const createStartingTemplate = function(){
 			<div class="my-contact">
 				<h1> Contact Information </h1>
 				<h2> Shannon McNellis </h2>
-				<h2> shannonmcnellis@gmail.com </h2>
+				<h2> <a href="mailto:shannmcnelli@gmail.com?subject=Portfolio">shannmcnelli@gmail.com</a></h2>
+				<a class= "icon" href="https://github.com/smcnell"><i class="fa fa-github" aria-hidden="true"></i></a>
+				<a class= "icon" href="https://www.linkedin.com/in/shannon-mcnellis-291815138/"><i class="fa fa-linkedin-square" aria-hidden="true"></i> </a>
+				<a class= "icon" href="https://twitter.com/shannonmcnellis"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+
 			</div>
 		</div>
 
-	<div id="about" class="about-me">
-		<div class= "big-container">
-		<h1> About </h1>
-			<div class= "about">
-				<img src ="./headshotcopy.jpg"/>
-				<p>Hi, I'm Shannon.  I am a recent graduate of The Iron Yard of Charleston, where my love for problem-solving and creating took shape in the form of coding for the first time.  I am a former Special Education teacher and will forever be inspired by individuality.</p>
-			</div>
-			<div class= "footer-container">
-				<a class= "icon" href="https://github.com/smcnell"><i class="fa fa-github" aria-hidden="true"></i> <p>View @smcnell on GitHub for my current projects and explorations.</p></a>
-				<a class= "icon" href="https://www.linkedin.com/in/shannon-mcnellis-291815138/"><i class="fa fa-linkedin-square" aria-hidden="true"></i> <p>Connect with me using LinkedIn</p></a>
-				<a class= "icon" href="https://twitter.com/shannonmcnellis"><i class="fa fa-twitter-square" aria-hidden="true"></i><p>Follow Me on Twitter</p></a>
-			</div>
-		</div>
-	</div>
 
 	`
 
