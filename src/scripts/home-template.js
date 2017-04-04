5c1cff05639949ee0c2ccbbbf5c6638ab50d190d
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-04-03 22:12:19 -0400

    border changes etc

--- src/scripts/home-template.js
+++ src/scripts/home-template.js
@@ -17,28 +17,14 @@ export const createStartingTemplate = function(){
 			<h2> Front-End Engineer </h2>
 		</div>
 	</div>
-	<br/>
 
-	<div id="about" class="about-me">
-		<div class= "big-container">
-				<div class= "about-title-container">
-					<img src= "./ironspiral3.jpg"/>
-					<h1> About </h1>
-					<img src= "./ironspiralflipped3.jpg"/>
-				</div>
-			<div class= "about">
-				<img src ="./headshotcopy.jpg"/>
-				<p>Hi, I'm Shannon, and I'm a Front-End Engineer.  I am a recent graduate of The Iron Yard of Charleston, where my love for problem-solving and creating took shape in the form of coding for the first time.  I am a former Special Education teacher and will forever be inspired by individuality.</p>
-			</div>
-		</div>
-	</div>
+
+
 
 
 <div id="skills" class="skills">
 <div class= "about-title-container">
-	<img src= "./ironspiral3.jpg"/>
 	<h1> Skills </h1>
-	<img src= "./ironspiralflipped3.jpg"/>
 </div>
 
 	<div class= "skills-container">
@@ -64,7 +50,6 @@ export const createStartingTemplate = function(){
 				<p> Mobile digital media time is higher than desktops, and the difference keeps growing.  Using frameworks such as Bootstrap, I use a mobile-first approach to ensure quality for all devices available now and for those to come.</p>
 			</div>
 			<div class="jumbotron triangles-jumbo">
-				<img src="./triangles.png" />
 			</div>
 
 		</div>
@@ -73,38 +58,19 @@ export const createStartingTemplate = function(){
 
 
 <div id="projects" class="projects">
+<h1> Projects </h1>
+
 	<div class="row">
 
-	<div class="col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<img src="./Iceland_home.png" alt="...">
-			<div class="caption">
-				<h3>Iceland API</h3>
-				<p>Summary: </p>
-				<p>Technologies: </p>
-				<p> Link: </p>
-			</div>
-		</div>
-	</div>
 
-	<div class="col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<img src="./BBC_Radio.png" alt="...">
-			<div class="caption">
-				<h3>BBC Radio</h3>
-				<p>Summary: </p>
-				<p>Technologies: </p>
-				<p> Link: </p>
-			</div>
-		</div>
-	</div>
+
 
 	<div class="col-sm-6 col-md-4">
 		<div class="thumbnail">
 			<img src="./GitHub_home.png" alt="...">
 			<div class="caption">
 				<h3>GitHub Remake</h3>
-				<p>Summary: </p>
+				<p>Using GitHub's public API,  </p>
 				<p>Technologies: </p>
 				<p> Link: </p>
 			</div>
@@ -152,16 +118,29 @@ export const createStartingTemplate = function(){
 	</div>
 </div>
 
+<div id="about" class="about-me">
+	<div class= "big-container">
+			<div class= "about-title-container">
+				<img src= "./ironspiral3.jpg"/>
+				<h1> About Me</h1>
+				<img src= "./ironspiralflipped3.jpg"/>
+			</div>
+		<div class= "about">
+			<img src ="./headshotcopy.jpg"/>
+			<p>Hi, I'm Shannon, and I'm a Front-End Engineer.  I am a recent graduate of The Iron Yard of Charleston, where my love for problem-solving and creating took shape in the form of coding for the first time.  I am a former Special Education teacher and will forever be inspired by individuality.</p>
+		</div>
+	</div>
+</div>
 
 	<div id="contact" class="contact">
 		<div class="jumbotron circle-jumbo">
-			<a href="http://pixady.com/image/ml3/"><img src="http://img.pixady.com/2017/03/175239_bluecircles_460x345.jpg" alt="" /></a>
+			<a href="http://pixady.com/image/ml3/"><img src="./greencircles.jpg" alt="" /></a>
 			<div class="my-contact">
 				<h1> Contact Information </h1>
 				<h2> Shannon McNellis </h2>
 				<h2> <a href="mailto:shannmcnelli@gmail.com?subject=Portfolio">shannmcnelli@gmail.com</a></h2>
 				<a class= "icon" href="https://github.com/smcnell"><i class="fa fa-github" aria-hidden="true"></i></a>
-				<a class= "icon" href="https://www.linkedin.com/in/shannon-mcnellis-291815138/"><i class="fa fa-linkedin-square" aria-hidden="true"></i> </a>
+				<a class= "icon" href="https://www.linkedin.com/in/shannon-mcnellis-291815138/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
 				<a class= "icon" href="https://twitter.com/shannonmcnellis"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
 
 			</div>
